ff3d299abfac21bdefe81f86d8bc8de9ea2d6aec
Stephen Zweibel <srzweibel@gmail.com>
2014-02-21 15:01:02 -0500

    added git install

--- install_dhbox.sh
+++ install_dhbox.sh
@@ -11,14 +11,11 @@ if [ -d $INSTALL_DIR ]; then
   echo "Looks like you have a $INSTALL_DIR directory installed.  Good job!"
   exit
 fi;
-
 # Gotta have git, and dependencies!
 apt-get install libcurl4-gnutls-dev libexpat1-dev gettext \
   libz-dev libssl-dev git
 yes
-
 git clone git://github.com/szweibel/dhbox.git $INSTALL_DIR
-
 for x in $HOME/.bashrc $HOME/.profile $HOME/.bash_profile ; do
   if [ -e $x ]; then
     mv $x "$x"_backup
