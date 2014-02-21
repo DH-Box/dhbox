1bbd056134f9a829e138fb35f53c49c3d3fc3500
Stephen Zweibel <srzweibel@gmail.com>
2014-02-21 15:32:03 -0500

    added git install

--- install_dhbox.sh
+++ install_dhbox.sh
@@ -11,9 +11,16 @@ if [ -d $INSTALL_DIR ]; then
   echo "Looks like you have a $INSTALL_DIR directory installed.  Good job!"
   exit
 fi;
-# Gotta have git, and dependencies!
+# Gotta have git
 apt-get install -y git
+
+# Install our scripts
 git clone git://github.com/szweibel/dhbox.git $INSTALL_DIR
+
+# install pip, the python package manager
+wget --no-check-certificate https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+python get-pip.py
+
 for x in $HOME/.bashrc $HOME/.profile $HOME/.bash_profile ; do
   if [ -e $x ]; then
     mv $x "$x"_backup
