20c861e1be3ca8146f93266dc993299fe660b686
Stephen Zweibel <srzweibel@gmail.com>
2014-02-26 16:01:25 -0500

    adds difference between ubuntu and debian

--- install_dhbox.sh
+++ install_dhbox.sh
@@ -40,43 +40,34 @@ esac
 echo "$OS is the OS"
 
 if [ "$OS" = "Linux" ]; then
-    apt-get update
-    # Gotta have git, and bash completion. Checking if it already exists.
-    if ! type "$git" > /dev/null;
-      then
-        apt-get install -y git-core bash-completion python-zmq
+    if [ ! type "$sudo" > /dev/null ]
+      apt-get update
+      # Gotta have git, and bash completion. Checking if it already exists.
+      if ! type "$git" > /dev/null;
+        then
+          apt-get install -y git-core bash-completion python-zmq
+      fi
+
+      # install pip, the python package manager
+      wget --no-check-certificate https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+      python get-pip.py
+
+      yes | pip install nltk ipython[all]
+    else
+      sudo apt-get update
+      # Gotta have git, and bash completion. Checking if it already exists.
+      if ! type "$git" > /dev/null;
+        then
+          sudo apt-get install -y git-core bash-completion python-zmq
+      fi
+
+      # install pip, the python package manager
+      sudo wget --no-check-certificate https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+      python get-pip.py
+
+      yes | sudo pip install nltk ipython[all]
     fi
-
-    # install pip, the python package manager
-    wget --no-check-certificate https://raw.github.com/pypa/pip/master/contrib/get-pip.py
-    python get-pip.py
-
-    yes | pip install nltk ipython[all]
-
 elif [ "$OS" = "Darwin" ]; then
-    # get xcode command line tools
-
-    # OSX_VERS=$(sw_vers -productVersion | awk -F "." '{print $2}')
-
-    # # Get Xcode CLI tools
-    # # https://devimages.apple.com.edgekey.net/downloads/xcode/simulators/index-3905972D-B609-49CE-8D06-51ADC78E07BC.dvtdownloadableindex
-    # TOOLS=clitools.dmg
-    # if [ ! -f "$TOOLS" ]; then
-    #   if [ "$OSX_VERS" -eq 7 ]; then
-    #       DMGURL=http://devimages.apple.com/downloads/xcode/command_line_tools_for_xcode_os_x_lion_april_2013.dmg
-    #   elif [ "$OSX_VERS" -eq 8 ]; then
-    #       DMGURL=http://devimages.apple.com/downloads/xcode/command_line_tools_for_xcode_os_x_mountain_lion_april_2013.dmg
-    #   elif [ "$OSX_VERS" -eq 9 ]; then
-    #       DMGURL=http://adcdownload.apple.com/Developer_Tools/command_line_tools_os_x_mavericks_for_xcode__late_october_2013/command_line_tools_os_x_mavericks_for_xcode__late_october_2013.dmg
-    #   fi
-    #   curl "$DMGURL" -o "$TOOLS"
-    # fi
-    # TMPMOUNT=`/usr/bin/mktemp -d /tmp/clitools.XXXX`
-    # hdiutil attach "$TOOLS" -mountpoint "$TMPMOUNT"
-    # installer -pkg "$(find $TMPMOUNT -name '*.mpkg')" -target /
-    # hdiutil detach "$TMPMOUNT"
-    # rm -rf "$TMPMOUNT"
-    # rm "$TOOLS"
 
     # install Mac Homebrew for easy installation of other stuff. Check if it exists.
     if ! type "$brew" > /dev/null;
@@ -133,6 +124,9 @@ echo "source ~/.bashrc"
 # Making the dhbox virtualenv
 # mkvirtualenv dhbox
 
+
+# Install the demo texts
+python -m nltk.downloader book
 
 echo 'got it!'
 ipython notebook $DHBOX_INSTALL_DIR/test.ipynb
