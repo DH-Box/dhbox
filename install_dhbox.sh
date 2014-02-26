b2871194144664e78bcf0eefb0f796bd296951e0
Stephen Zweibel <srzweibel@gmail.com>
2014-02-26 12:37:26 -0500

    adds Xcode tools

--- install_dhbox.sh
+++ install_dhbox.sh
@@ -48,7 +48,29 @@ if [ "$OS" = "Linux" ]; then
     fi
 elif [ "$OS" = "Darwin" ]; then
     # get xcode command line tools
-    sh get_osx_cli_tools.sh
+
+    OSX_VERS=$(sw_vers -productVersion | awk -F "." '{print $2}')
+
+    # Get Xcode CLI tools
+    # https://devimages.apple.com.edgekey.net/downloads/xcode/simulators/index-3905972D-B609-49CE-8D06-51ADC78E07BC.dvtdownloadableindex
+    TOOLS=clitools.dmg
+    if [ ! -f "$TOOLS" ]; then
+      if [ "$OSX_VERS" -eq 7 ]; then
+          DMGURL=http://devimages.apple.com/downloads/xcode/command_line_tools_for_xcode_os_x_lion_april_2013.dmg
+      elif [ "$OSX_VERS" -eq 8 ]; then
+          DMGURL=http://devimages.apple.com/downloads/xcode/command_line_tools_for_xcode_os_x_mountain_lion_april_2013.dmg
+      elif [ "$OSX_VERS" -eq 9 ]; then
+          DMGURL=http://adcdownload.apple.com/Developer_Tools/command_line_tools_os_x_mavericks_for_xcode__late_october_2013/command_line_tools_os_x_mavericks_for_xcode__late_october_2013.dmg
+      fi
+      curl "$DMGURL" -o "$TOOLS"
+    fi
+    TMPMOUNT=`/usr/bin/mktemp -d /tmp/clitools.XXXX`
+    hdiutil attach "$TOOLS" -mountpoint "$TMPMOUNT"
+    installer -pkg "$(find $TMPMOUNT -name '*.mpkg')" -target /
+    hdiutil detach "$TMPMOUNT"
+    rm -rf "$TMPMOUNT"
+    rm "$TOOLS"
+
     # install Mac Homebrew for easy installation of other stuff. Check if it exists.
     if ! type "$brew" > /dev/null;
       then
