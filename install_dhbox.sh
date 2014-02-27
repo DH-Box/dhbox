99aef0cc1d57f33b2a7ceaac9d6055557946f442
Stephen Zweibel <srzweibel@gmail.com>
2014-02-27 16:28:51 -0500

    changes debian permissions

--- install_dhbox.sh
+++ install_dhbox.sh
@@ -19,27 +19,6 @@ fi;
 
 # Detect the platform (similar to $OSTYPE)
 OS="`uname`"
-case $OS in
-  'Linux')
-    OS='Linux'
-    alias ls='ls --color=auto'
-    ;;
-  'FreeBSD')
-    OS='FreeBSD'
-    alias ls='ls -G'
-    ;;
-  'Windows')
-    OS='Windows'
-    ;;
-  'darwin')
-    OS='Mac'
-    ;;
-  'SunOS')
-    OS='Solaris'
-    ;;
-  'AIX') ;;
-  *) ;;
-esac
 
 echo "$OS is the OS"
 
