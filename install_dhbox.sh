66238e326f474326e7d2be9de789ef2748a6a40a
Stephen Zweibel <srzweibel@gmail.com>
2014-02-27 22:45:05 -0500

    fighting with debian

--- install_dhbox.sh
+++ install_dhbox.sh
@@ -147,11 +147,12 @@ do
       cp $x "$x"_backup
       # Add our scripts
       echo "DHBOX_INSTALL_DIR=$HOME/.dhbox" >> $x
+      echo ". $DHBOX_INSTALL_DIR/dhbox.sh" >> $x
+      . $x
     fi;
 done
 # Reloading startup file
 # if $THE_OS = Ubuntu ; then
-echo ". $DHBOX_INSTALL_DIR/dhbox.sh" >> $HOME/.bashrc
 echo ". $DHBOX_INSTALL_DIR/dhbox.sh" >> $HOME/.profile
 . $HOME/.profile
 . $HOME/.bashrc
