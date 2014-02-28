c5c87cdd1d2a152b17eb88987c4673d7d77912bf
Stephen Zweibel <srzweibel@gmail.com>
2014-02-28 12:02:29 -0500

    Works for Ubuntu and Debian!

--- install_dhbox.sh
+++ install_dhbox.sh
@@ -19,27 +19,27 @@ fi;
 
 # Detect the platform (similar to $OSTYPE)
 OS="`uname`"
-case $OS in
-  'Linux')
-    OS='Linux'
-    alias ls='ls --color=auto'
-    ;;
-  'FreeBSD')
-    OS='FreeBSD'
-    alias ls='ls -G'
-    ;;
-  'Windows')
-    OS='Windows'
-    ;;
-  'darwin')
-    OS='Mac'
-    ;;
-  'SunOS')
-    OS='Solaris'
-    ;;
-  'AIX') ;;
-  *) ;;
-esac
+# case $OS in
+#   'Linux')
+#     OS='Linux'
+#     alias ls='ls --color=auto'
+#     ;;
+#   'FreeBSD')
+#     OS='FreeBSD'
+#     alias ls='ls -G'
+#     ;;
+#   'Windows')
+#     OS='Windows'
+#     ;;
+#   'darwin')
+#     OS='Mac'
+#     ;;
+#   'SunOS')
+#     OS='Solaris'
+#     ;;
+#   'AIX') ;;
+#   *) ;;
+# esac
 
 echo "$OS is the OS"
 
@@ -142,25 +142,25 @@ elif [ "$OS" = "Darwin" ]; then
     # Get correct permissions for Homebrew
     sudo chown 'whoami' usr/local/lib/
     # install Mac Homebrew for easy installation of other stuff.
-    if ! type "$brew" > /dev/null;
+    if ! hash brew 2>/dev/null;
       then
         ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
     fi
     # Gotta have git. Check for it.
-    if ! type "$git" > /dev/null;
+    if ! hash git 2>/dev/null;
       then
         brew install git
     fi
-    if ! type "$wget" > /dev/null;
+    if ! hash wget 2>/dev/null;
       then
         brew install wget
     fi
 
-    if ! type "$pip" > /dev/null;
+    if ! hash pip 2>/dev/null;
       then
         sudo easy_install pip
     fi
-    . $HOME/.bash_profile
+
     # Installing virtualenv and virtualenvwrapper
     sudo pip install virtualenv
     sudo pip install virtualenvwrapper
@@ -179,6 +179,7 @@ elif [ "$OS" = "Darwin" ]; then
     # Install our scripts
     git clone git://github.com/szweibel/dhbox.git $DHBOX_INSTALL_DIR
 
+    VIRTLOCATION="which virtualenvwrapper.sh"
     # Make backups of bash configuration files
     for x in $HOME/.bashrc $HOME/.profile $HOME/.bash_profile ;
     do
@@ -187,13 +188,12 @@ elif [ "$OS" = "Darwin" ]; then
           # Add our scripts
           echo "DHBOX_INSTALL_DIR=$HOME/.dhbox" >> $x
           echo ". $DHBOX_INSTALL_DIR/dhbox.sh" >> $x
-          # echo "export WORKON_HOME=$HOME/.virtualenvs" >> $x
-          # echo ". $VIRTLOCATION" >> $x
+          echo "export WORKON_HOME=$HOME/.virtualenvs" >> $x
+          echo ". $VIRTLOCATION" >> $x
         fi;
     done
     # Reloading startup file
-    # . $HOME/.bash_profile
-    # . $HOME/.bashrc
+    . $HOME/.bash_profile
     # Delete all .pyc files?
     # find / -iname \*.pyc -exec rm {} \;
     # Install the demo texts
@@ -202,6 +202,5 @@ elif [ "$OS" = "Darwin" ]; then
     ipython notebook $HOME/.dhbox/notebooks/the-waves
 else
   echo "Unfortunately $OS isn't supported yet. Exiting..."
-  exit
 fi
 exit
