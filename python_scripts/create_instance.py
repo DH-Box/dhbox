bdde95db98d5afacc789bd1780c65e1c0dfb997c
Stephen Zweibel <srzweibel@gmail.com>
2014-03-16 18:46:08 -0400

    add user data

---
+++ python_scripts/create_instance.py
@@ -0,0 +1,15 @@
+import boto
+import boto.ec2
+import urllib2
+
+req = urllib2.Request("https://raw.github.com/DH-Box/dhbox/master/install_box.sh")
+opener = urllib2.build_opener()
+f = opener.open(req)
+
+conn = boto.ec2.connect_to_region("us-east-1")
+
+user_data = f.read()
+print user_data
+
+conn.run_instances('ami-d3675dba', key_name='stevess', security_groups=['default'], instance_type='t1.micro', user_data=user_data)
+
---
+++ python_scripts/create_stack.py
@@ -0,0 +1,13 @@
+import boto
+import urllib2
+import json
+
+req = urllib2.Request("https://raw.github.com/DH-Box/dhbox/master/dhbox_test.json")
+opener = urllib2.build_opener()
+f = opener.open(req)
+data = json.load(f)
+template = json.dumps(data)
+
+c = boto.connect_cloudformation()
+c.create_stack(template_body=template, stack_name='DHBox', parameters=[('KeyName', 'stevess')])
+
