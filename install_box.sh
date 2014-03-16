cb233e5ff9b21a1c2fd9492ce961d745015912fe
Stephen Zweibel <srzweibel@gmail.com>
2014-03-16 13:12:27 -0400

    add startup script

--- install_box.sh
+++ install_box.sh
@@ -1,7 +1,11 @@
 #! /bin/sh
 # /etc/init.d/startup
 #
+
+# Update packages
 apt-get update && apt-get dist-upgrade
+
+# Install Amazon Cloud scripts
 wget --no-check-certificate -P /root https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
 mkdir -p /root/aws-cfn-bootstrap-latest
 tar xvfz /root/aws-cfn-bootstrap-latest.tar.gz --strip-components=1 -C /root/aws-cfn-bootstrap-latest
@@ -10,3 +14,10 @@ sudo cfn-init -s DHBox --region us-east-1 -r NewServer
 sudo rm /etc/init.d/startup.sh
 
 sudo service shellinabox start
+
+
+
+
+wget --no-check-certificate https://raw.github.com/DH-Box/dhbox/master/install_box.sh
+
+wget --no-check-certificate https://raw.github.com/DH-Box/dhbox/master/install_box.sh -O - | sh
