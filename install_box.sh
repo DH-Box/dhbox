ed56e2a0244779c3ab2c68d71da8327265eb7ab1
Stephen Zweibel <srzweibel@gmail.com>
2014-03-16 20:53:34 -0400

    testing template

--- install_box.sh
+++ install_box.sh
@@ -18,8 +18,8 @@ echo "Installing ipython"
 yes | sudo pip install ipython[all]
 
 # Install MALLET
-# sudo wget --no-check-certificate -P /dhbox http://mallet.cs.umass.edu/dist/mallet-2.0.7.tar.gz
-# sudo tar xvfz /dhbox/mallet-2.0.7.tar.gz --strip-components=1 -C /dhbox/mallet
-# sudo ant -buildfile /dhbox/mallet/build.xml
+sudo wget --no-check-certificate -P /dhbox http://mallet.cs.umass.edu/dist/mallet-2.0.7.tar.gz
+sudo tar xvfz /dhbox/mallet-2.0.7.tar.gz --strip-components=1 -C /dhbox/mallet
+sudo ant -buildfile /dhbox/mallet/build.xml
 
 sudo reboot
