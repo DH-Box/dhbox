79ce11c3d052998bc58069c88d279d461fb29815
Stephen Zweibel <srzweibel@gmail.com>
2014-03-17 09:17:02 -0400

    add template generator

--- install_box.sh
+++ install_box.sh
@@ -2,22 +2,17 @@
 #
 echo "Update packages"
 sudo apt-get update -y && sudo apt-get dist-upgrade -y && sudo apt-get upgrade -y && sudo apt-get install -y unattended-upgrades python-pip
-
 echo "Make a source directory for DHBox"
 sudo mkdir /dhbox
 sudo mkdir /dhbox/mallet
-
 echo "Installing Amazon Cloud scripts"
 sudo wget --no-check-certificate -P /root https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
 sudo mkdir -p /root/aws-cfn-bootstrap-latest
 sudo tar xvfz /root/aws-cfn-bootstrap-latest.tar.gz --strip-components=1 -C /root/aws-cfn-bootstrap-latest
 sudo easy_install /root/aws-cfn-bootstrap-latest
 sudo cfn-init -s DHBox --region us-east-1 -r NewServer -v
-sudo reboot
-
 echo "Installing ipython"
 yes | sudo pip install ipython[all]
-
 # Install MALLET
 sudo wget --no-check-certificate -P /dhbox http://mallet.cs.umass.edu/dist/mallet-2.0.7.tar.gz
 sudo tar xvfz /dhbox/mallet-2.0.7.tar.gz --strip-components=1 -C /dhbox/mallet
