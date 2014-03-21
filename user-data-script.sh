8159805b6fa97ca1be58e115f571ec703a93c8cc
Stephen Zweibel <srzweibel@gmail.com>
2014-03-21 13:42:48 -0400

    moves to Puppet

--- user-data-script.sh
+++ user-data-script.sh
@@ -3,18 +3,22 @@ set -e -x
 
 sudo apt-get --yes --quiet update
 sudo apt-get --yes --quiet install git puppet-common
+sudo mv /etc/puppet /etc/puppet.orig
+
+
+sudo apt-get --yes --quiet install git default-jdk
+
+
 
 # Make a source directory for DHBox (and MALLET)
 sudo mkdir -p /dhbox/mallet
-
-sudo puppet module install maestrodev-ant
-sudo puppet module install maestrodev-wget
 #
 # Fetch puppet configuration from public git repository.
 #
 
-sudo mv /etc/puppet /etc/puppet.orig
 sudo git clone $puppet_source /etc/puppet
+sudo puppet module install maestrodev-ant --force
+sudo puppet module install maestrodev-wget --force
 
 #
 # Run puppet.
@@ -28,5 +32,5 @@ sudo chmod -R 777 /usr/local/projects/Omeka/
 # Install MALLET
 #
 sudo wget --no-check-certificate -P /dhbox http://mallet.cs.umass.edu/dist/mallet-2.0.7.tar.gz
-sudo tar xvfz /dhbox/mallet-2.0.7.tar.gz --strip-components=1 -C /dhbox/mallet
+sudo tar xfz /dhbox/mallet-2.0.7.tar.gz --strip-components=1 -C /dhbox/mallet
 sudo ant -buildfile /dhbox/mallet/build.xml
