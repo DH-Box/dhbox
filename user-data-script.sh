e9c9c60c3269a89d5fc73b098441c48c39ad04f1
Stephen Zweibel <srzweibel@gmail.com>
2014-03-21 14:36:54 -0400

    back to square 2

--- user-data-script.sh
+++ user-data-script.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 set -e -x
 
-sudo apt-get --yes --quiet update
-sudo apt-get --yes --quiet install git puppet-common default-jdk ant
-sudo mv /etc/puppet /etc/puppet.orig
+apt-get --yes --quiet update
+apt-get --yes --quiet install git puppet-common
 
-# Make a source directory for DHBox (and MALLET)
-sudo mkdir -p /dhbox/mallet
 #
 # Fetch puppet configuration from public git repository.
 #
 
-sudo git clone $puppet_source /etc/puppet
+mv /etc/puppet /etc/puppet.orig
+git clone $puppet_source /etc/puppet
+
 #
 # Run puppet.
 #
-sudo puppet apply /etc/puppet/manifests/init.pp
-#
-# Change permissions so Omeka can run
-#
-sudo chmod -R 777 /usr/local/projects/Omeka/
+
+puppet apply /etc/puppet/manifests/init.pp
+
+chmod -R 777 /usr/local/projects/Omeka/
+
 #
 # Install MALLET
 #
-sudo wget --no-check-certificate -P /dhbox http://mallet.cs.umass.edu/dist/mallet-2.0.7.tar.gz
-sudo tar xfz /dhbox/mallet-2.0.7.tar.gz --strip-components=1 -C /dhbox/mallet
-sudo ant -buildfile /dhbox/mallet/build.xml
+mkdir -p /dhbox/mallet
+wget --no-check-certificate -P /dhbox http://mallet.cs.umass.edu/dist/mallet-2.0.7.tar.gz
+tar xfz /dhbox/mallet-2.0.7.tar.gz --strip-components=1 -C /dhbox/mallet
+ant -buildfile /dhbox/mallet/build.xml
