840f0d8eaa2295953bc291da438f5ca15b00eb36
Stephen Zweibel <srzweibel@gmail.com>
2014-03-27 10:30:57 -0400

    experiment

--- backend.py
+++ backend.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, request, redirect, url_for, render_template, flash, make_response, jsonify, send_file
+from flask import Flask, request, redirect, url_for, render_template, flash, make_response, jsonify, send_file, current_app
 import launch_instance
+from datetime import timedelta
+from functools import update_wrapper
 
 # create application
 app = Flask('dhbox')
@@ -8,9 +10,52 @@ app = Flask('dhbox')
 """
 URLS/VIEWS
 """
+# decorator
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
 
 # API
 @app.route('/dhbox', methods=['POST'])
+@crossdomain(origin='*')
 def dhbox():
     admin = request.form['admin']
     users = request.form.getlist('users')  # Get every form item with name='users' and create a list
