a7ab312abb7c7860f327316eb0a083a7d3aac798
Stephen Zweibel <srzweibel@gmail.com>
2014-04-12 14:52:35 -0400

    email fixes

--- backend/emailing.py
+++ backend/emailing.py
@@ -1,32 +1,36 @@
 import smtplib
 import sys
 from string import Template
+import ConfigParser
 
 def make_an_email(who, ip_address, filename='../email.txt'):
-    to = who
-    gmail_user = 'do-not-reply@dhbox.org'
-    gmail_pwd = 'GCProfGold4'
-    smtpserver = smtplib.SMTP("smtp.live.com",587)
+    
+    config = ConfigParser.ConfigParser()
+    config.read("../settings.cfg")
+    gmail_user = str(config.get('config', 'MAIL_USERNAME'))
+    gmail_pwd = str(config.get('config', 'MAIL_PASSWORD'))
+    smtpserver = smtplib.SMTP("smtp.gmail.com",587)
     smtpserver.ehlo()
     smtpserver.starttls()
     smtpserver.ehlo
     smtpserver.login(gmail_user, gmail_pwd)
+    to = who 
     header = 'To:' + to + '\n' + 'From: ' + gmail_user + '\n' + 'Subject:testing \n'
     print header
     template = open(filename).read()
     msg = Template(template).substitute(ip_address=ip_address)
     msg = header + '\n '+msg+' \n\n'
     smtpserver.set_debuglevel(1)
-    try:
-        smtpserver.starttls() 
-        smtpserver.login(login, password) 
-        smtpserver.sendmail(gmail_user, to, msg)
-    finally:
-        smtpserver.quit()
+    # try:
+    #     smtpserver.starttls() 
+    #     smtpserver.login(login, password) 
+    #     smtpserver.sendmail(gmail_user, to, msg)
+    # finally:
+    #     smtpserver.quit()
 
-    # smtpserver.sendmail(gmail_user, to, msg)
-    # print 'done!'
-    # smtpserver.close()
+    smtpserver.sendmail(gmail_user, to, msg)
+    print 'done!'
+    smtpserver.close()
 
 
 if __name__ == '__main__':
