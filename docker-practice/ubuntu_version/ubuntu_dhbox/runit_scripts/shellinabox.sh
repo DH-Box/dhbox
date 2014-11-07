1b58b390305d5760820ff19970d1473b24d31b13
Stephen Zweibel <srzweibel@gmail.com>
2014-11-07 11:42:12 -0500

    working on transfer

--- docker-practice/ubuntu_version/ubuntu_dhbox/runit_scripts/shellinabox.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-### In rserver.sh (make sure this file is chmod +x):
-# `/sbin/setuser xxxx` runs the given command as the user `xxxx`.
-# If you omit that part, the command will be run as root.
-sleep 5
-echo "Starting Shellinabox"
-# sudo service shellinabox start >>/var/log/shellinabox.log 2>&1
-shellinaboxd -t >>/var/log/shellinabox.log 2>&1
