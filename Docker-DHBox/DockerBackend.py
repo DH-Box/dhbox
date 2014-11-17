17098ecd2aa17e3170fec1a35d38f9c7972c1b89
Stephen Zweibel <srzweibel@gmail.com>
2014-11-16 21:14:48 -0500

    adding requirements

--- Docker-DHBox/DockerBackend.py
+++ Docker-DHBox/DockerBackend.py
@@ -5,7 +5,7 @@ import os
 
 def attach_to_docker_client():
 	docker_host = os.environ['DOCKER_HOST']
-	if docker_host == 'tcp://192.168.59.103:2376':
+	if docker_host and if docker_host == 'tcp://192.168.59.103:2376':
 		c = Client(**kwargs_from_env(assert_hostname=False))
 	else:
 		c = Client()
