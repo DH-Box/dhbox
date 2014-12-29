891eaeb0991eaaf31e8d69d1fbf7b12b1280cca6
Stephen Zweibel <srzweibel@gmail.com>
2014-12-29 18:16:25 -0500

    more work with ports

---
+++ Docker-DHBox/database/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=False)
+    email = Column(String(120), unique=False)
+    ip = Column(String(50), unique=False)
+
+    def __init__(self, name=None, email=None, ip=None):
+        self.name = name
+        self.email = email
+        self.ip = ip
+
+    def __repr__(self):
+        return '<User %r>' % (self.name)
