f070a89b2f100e42603d1122db67a9a927da77d2
Stephen Zweibel <srzweibel@gmail.com>
2015-01-12 15:52:52 -0500

    key

--- Docker-DHBox/site.py
+++ Docker-DHBox/site.py
@@ -15,26 +15,26 @@ import DockerBackend
 # create application
 app = Flask('dhbox')
 
-def install_secret_key(app, filename='secret_key'):
-    """Configure the SECRET_KEY from a file
-    in the instance directory.
+# def install_secret_key(app, filename='secret_key'):
+#     """Configure the SECRET_KEY from a file
+#     in the instance directory.
 
-    If the file does not exist, print instructions
-    to create it from a shell with a random key,
-    then exit.
+#     If the file does not exist, print instructions
+#     to create it from a shell with a random key,
+#     then exit.
 
-    """
-    filename = os.path.join(app.instance_path, filename)
-    try:
-        app.config['SECRET_KEY'] = open(filename, 'rb').read()
-    except IOError:
-        print 'Error: No secret key. Create it with:'
-        if not os.path.isdir(os.path.dirname(filename)):
-            print 'mkdir -p', os.path.dirname(filename)
-        print 'head -c 24 /dev/urandom >', filename
-        sys.exit(1)
+#     """
+#     filename = os.path.join(app.instance_path, filename)
+#     try:
+#         app.config['SECRET_KEY'] = open(filename, 'rb').read()
+#     except IOError:
+#         print 'Error: No secret key. Create it with:'
+#         if not os.path.isdir(os.path.dirname(filename)):
+#             print 'mkdir -p', os.path.dirname(filename)
+#         print 'head -c 24 /dev/urandom >', filename
+#         sys.exit(1)
 
-install_secret_key(app)
+# install_secret_key(app)
 app.config.from_pyfile('config.cfg')
 
 # Create database connection object
