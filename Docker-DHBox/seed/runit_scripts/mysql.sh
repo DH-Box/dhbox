9c372d53416b2a3e6da7c5501a87f6dc258c27f0
Stephen Zweibel <srzweibel@gmail.com>
2015-01-21 16:26:37 -0500

    using docker exec

--- Docker-DHBox/seed/runit_scripts/mysql.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -e
-echo "starting mysql"
-exec 2>&1
-exec mysqld_safe --console --user=mysql
--- Docker-DHBox/seed/runit_scripts/rserver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-### In rserver.sh (make sure this file is chmod +x):
-# `/sbin/setuser xxxx` runs the given command as the user `xxxx`.
-# If you omit that part, the command will be run as root.
-echo "Starting R-Studio"
-exec sudo rstudio-server start
-exec  /usr/lib/rstudio-server/bin/rserver >>/var/log/rserver.log 2>&1
