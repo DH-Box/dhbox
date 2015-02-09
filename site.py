d16a82b33159c872076ff6c4d1eea802e7e1c307
Stephen Zweibel <srzweibel@gmail.com>
2015-02-09 12:10:36 -0500

    gunicorn

--- site.py
+++ site.py
@@ -241,5 +241,7 @@ def kill_dhbox():
 
 if __name__ == '__main__':
 	app.debug = True
-	# app.run(host='0.0.0.0', port=port, threaded=True)
-	app.run()
+	# Bind to PORT if defined, otherwise default to 5000.
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port, threaded=True)
+	# app.run()
