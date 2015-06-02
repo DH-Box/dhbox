08570667f9d2b1f044efedb750b8f901c9fbd47c
Don Smith III <deansauce@gmail.com>
2015-06-01 22:15:51 -0600

    updated design again without horrible merge conflicts

--- DockerBackend.py
+++ DockerBackend.py
@@ -6,124 +6,142 @@ from urllib2 import urlopen
 import os, time, subprocess
 from threading import Timer
 import dhbox
+import logging
 
 default_hostname = json.load(urlopen('http://httpbin.org/ip'))['origin']
 
+
 def attach_to_docker_client():
-	if os.getenv('DOCKER_HOST') == 'tcp://192.168.59.103:2376':
-		c = Client(**kwargs_from_env(assert_hostname=False))
-	else:
-		c = Client()
-	return c
+    if os.getenv('DOCKER_HOST') == 'tcp://192.168.59.103:2376':
+        c = Client(**kwargs_from_env(assert_hostname=False))
+    else:
+        c = Client()
+    return c
+
 
 def get_hostname():
-	"""Determine the IP address of the host server"""
-	if os.getenv('DOCKER_HOST') == 'tcp://192.168.59.103:2376':
-		hostname = 'dockerhost'
-	else:
-		if dhbox.app.config['TESTING']:
-			hostname = 'localhost'
-		else:
-			hostname = default_hostname
-	return hostname
+    """Determine the IP address of the host server"""
+    if os.getenv('DOCKER_HOST') == 'tcp://192.168.59.103:2376':
+        hostname = 'dockerhost'
+    else:
+        if dhbox.app.config['TESTING']:
+            hostname = 'localhost'
+        else:
+            hostname = default_hostname
+    return hostname
+
 
 def build_dhbox(username='test'):
-	"""Builds a new Dh Box seed, renaming the old one if it exists"""
-	print "Building Seed"
-	images = c.images()
-	for image in images:
-		if image["RepoTags"] == ["dhbox/seed:latest"]:
-			image_id = image["Id"]
-			c.tag(image=image_id, repository='dhbox/seed', tag='older', force=True)
-	os.chdir('seed/')
-	for line in c.build(path='.', rm=True, tag='dhbox/seed:latest'):
-		print line
-	if "errorDetail" in line:
-		# There was an error, so kill the container and the image
-		pass
-	os.chdir('../')
+    """Builds a new Dh Box seed, renaming the old one if it exists"""
+    print "Building Seed"
+    images = c.images()
+    for image in images:
+        if image["RepoTags"] == ["dhbox/seed:latest"]:
+            image_id = image["Id"]
+            c.tag(image=image_id, repository='dhbox/seed', tag='older', force=True)
+    os.chdir('seed/')
+    for line in c.build(path='.', rm=True, tag='dhbox/seed:latest'):
+        print line
+    if "errorDetail" in line:
+        # There was an error, so kill the container and the image
+        pass
+    os.chdir('../')
+
 
 def all_containers():
-	info = c.containers(all=True)
-	return info
+    info = c.containers(all=True)
+    return info
+
 
 def get_container_info(which_container):
-	info = c.inspect_container(which_container)
-	return info
+    info = c.inspect_container(which_container)
+    return info
+
 
 def get_container_port(container_name, app_port):
-	container = get_container_info(container_name)
-	public_port = container['NetworkSettings']['Ports'][app_port+'/tcp'][0]['HostPort']
-	return public_port
+    container = get_container_info(container_name)
+    public_port = container['NetworkSettings']['Ports'][app_port + '/tcp'][0]['HostPort']
+    return public_port
+
 
 def get_all_exposed_ports(container_name):
-	container = get_container_info(container_name)
-	public_ports = container['NetworkSettings']['Ports']
-	public_ports_cleaned = {}
-	for inside_port, outside_port in public_ports.iteritems():
-		inside_port = inside_port [:-4]
-		outside_port = outside_port[0]['HostPort']
-		public_ports_cleaned[inside_port] = outside_port
-	return public_ports_cleaned
+    container = get_container_info(container_name)
+    public_ports = container['NetworkSettings']['Ports']
+    public_ports_cleaned = {}
+    for inside_port, outside_port in public_ports.iteritems():
+        inside_port = inside_port[:-4]
+        outside_port = outside_port[0]['HostPort']
+        public_ports_cleaned[inside_port] = outside_port
+    return public_ports_cleaned
+
 
 def setup_new_dhbox(username, password, email, demo=False):
-	"""Create a new DH Box container, customize it."""
-	try:
-		print "Creating Container"
-		container = c.create_container(image='dhbox/seed:latest', name=username,
-			ports=[(25, 'tcp'),(80, 'tcp'), (4200, 'tcp'), (8080, 'tcp'), (8787, 'tcp')], tty=True, stdin_open=True)
-	except docker.errors.APIError, e:
-		raise e
-	else:
-		print "Starting Container"
-		restart_policy = { "MaximumRetryCount": 10, "Name": "always"}
-		c.start(container, publish_all_ports=True, restart_policy=restart_policy)
-		configure_dhbox(username, password, email, demo=demo)
-		info = c.inspect_container(container)
-		return info
+    """Create a new DH Box container, customize it."""
+    try:
+        print "Creating Container"
+        container = c.create_container(image='dhbox/seed:latest', name=username,
+                                       ports=(lambda x: app['port'] for app in dhbox.all_apps if app['port'] != None),
+                                       tty=True, stdin_open=True)
+    except docker.errors.APIError, e:
+        raise e
+    else:
+        print "Starting Container"
+        restart_policy = {"MaximumRetryCount": 10, "Name": "always"}
+        c.start(container, publish_all_ports=True, restart_policy=restart_policy)
+        configure_dhbox(username, password, email, demo=demo)
+        info = c.inspect_container(container)
+        return info
+
 
 def execute(container, args):
-	"""Execute a list of arbitrary Bash commands inside a container"""
-	for arg in args:
-		print arg
-		c.execute(container, arg, stdout=True, stderr=True, tty=False)
+    """Execute a list of arbitrary Bash commands inside a container"""
+    for arg in args:
+        print arg
+        c.execute(container, arg, stdout=True, stderr=True, tty=False)
+
 
 def configure_dhbox(user, the_pass, email, demo=False):
-	"""Use Docker exec to SSH into a new container, customizing it for the user.
-	Adds the user to the UNIX instance, and Omeka. """
-	container = user
-	if demo:
-		user='demonstration'
-	user_add_strings = ['adduser --disabled-password --gecos "" '+user, 'usermod -a -G sudo '+user]
-	config = execute(container, user_add_strings)
-	if os.getenv('DOCKER_HOST') == 'tcp://192.168.59.103:2376':
-		subprocess.call('echo '+user+':'+the_pass+' | docker exec -i '+container+' chpasswd', shell=True)
-	else:
-		subprocess.call('echo '+user+':'+the_pass+' | sudo docker exec -i '+container+' chpasswd', shell=True)
-	omeka_string = 'wget -O /tmp/install.html --post-data "username={0}&password={1}&password_confirm={1}&super_email={2}&administrator_email={2}&site_title=DHBox&description=DHBox&copyright=2014&author=DHBOX&tag_delimiter=,&fullsize_constraint=800&thumbnail_constraint=200&square_thumbnail_constraint=200&per_page_admin=10&per_page_public=10&show_empty_elements=0&path_to_convert=/usr/bin&install_submit=Install" localhost:8080/install/install.php'.format(user, the_pass, email)
-	time.sleep(5)
-	execute(container, [omeka_string])
+    """Use Docker exec to SSH into a new container, customizing it for the user.
+    Adds the user to the UNIX instance, and Omeka. """
+    container = user
+    if demo:
+        user = 'demonstration'
+    user_add_strings = ['adduser --disabled-password --gecos "" ' + user, 'usermod -a -G sudo ' + user]
+    config = execute(container, user_add_strings)
+    if os.getenv('DOCKER_HOST') == 'tcp://192.168.59.103:2376':
+        subprocess.call('echo ' + user + ':' + the_pass + ' | docker exec -i ' + container + ' chpasswd', shell=True)
+    else:
+        subprocess.call('echo ' + user + ':' + the_pass + ' | sudo docker exec -i ' + container + ' chpasswd',
+                        shell=True)
+    omeka_string = 'wget -O /tmp/install.html --post-data "username={0}&password={1}&password_confirm={1}&super_email={2}&administrator_email={2}&site_title=DHBox&description=DHBox&copyright=2014&author=DHBOX&tag_delimiter=,&fullsize_constraint=800&thumbnail_constraint=200&square_thumbnail_constraint=200&per_page_admin=10&per_page_public=10&show_empty_elements=0&path_to_convert=/usr/bin&install_submit=Install" localhost:8080/install/install.php'.format(
+        user, the_pass, email)
+    time.sleep(5)
+    execute(container, [omeka_string])
+
 
 def demo_dhbox(username):
-	"""Make a demonstration DH Box with a random name. Expires after an hour."""
-	password = 'demonstration'
-	email = username+'@demo.com'
-	setup_new_dhbox(username, password, email, demo=True)
-	t = Timer(600.0, kill_and_remove_user, [username])
-	t.start() # after one hour, demo will be destroyed
+    """Make a demonstration DH Box with a random name. Expires after an hour."""
+    password = 'demonstration'
+    email = username + '@demo.com'
+    setup_new_dhbox(username, password, email, demo=True)
+    t = Timer(600.0, kill_and_remove_user, [username])
+    t.start()  # after one hour, demo will be destroyed
+
 
 def kill_dhbox(ctr_name):
-	"""Kill a running DH Box container"""
-	try:
-		print "Killing container."
-		c.kill(ctr_name)
-		c.remove_container(ctr_name)
-	except Exception, e:
-		raise e
+    """Kill a running DH Box container"""
+    try:
+        print "Killing container."
+        c.kill(ctr_name)
+        c.remove_container(ctr_name)
+    except Exception, e:
+        raise e
+
 
 def kill_and_remove_user(name):
-	kill_dhbox(name)
-	dhbox.delete_user(name)
+    kill_dhbox(name)
+    dhbox.delete_user(name)
+
 
 def delete_untagged():
     """Find the untagged images and remove them"""
@@ -142,8 +160,9 @@ def delete_untagged():
     if not found:
         print "Didn't find any untagged images to delete!"
 
+
 c = attach_to_docker_client()
 
 if __name__ == '__main__':
-	c = DockerBackend.attach_to_docker_client()
-	# setup_new_dhbox('test', 'password', 'test@gmail.com')
+    c = DockerBackend.attach_to_docker_client()
+# setup_new_dhbox('test', 'password', 'test@gmail.com')
--- dhbox.py
+++ dhbox.py
@@ -17,23 +17,40 @@ app.wsgi_app = ProxyFix(app.wsgi_app)
 
 # install_secret_key(app)
 app.config.from_pyfile('config.cfg')
-
+app.template_folder = 'src/templates' if app.config['TESTING'] else 'dist/templates'
+app.static_folder = 'src/static' if app.config['TESTING'] else 'dist/static'
 # Create database connection object
 db = SQLAlchemy(app)
 
-all_apps = {'rstudio': '8787', 'bash': '4200', 'omeka': '8080', 'apache': '80'}
+all_apps = [
+    {'name': 'ipython', 'wiki-page': 'IPython', 'display-name': 'IPython'},
+    {'name': 'mallet', 'wiki-page': 'MALLET', 'display-name': 'MALLET'},
+    {'name': 'ntlk', 'wiki-page': 'NLTK', 'display-name': 'NLTK'},
+    {'name': 'bash', 'port': '4200', 'wiki-page': 'Bash-shell', 'display-name': 'Bash Shell'},
+    {'name': 'rstudio', 'port': '8787', 'wiki-page': 'R-Studio', 'display-name': 'R Studio'},
+    {'name': 'omeka', 'port': '8080', 'wiki-page': 'Omeka', 'display-name': 'Omeka'},
+    {'name': 'brackets', 'port': '4444', 'wiki-page': 'Brackets', 'display-name': 'Brackets'},
+    {'name': 'apache', 'port': '80', 'hide': True}
+]
+
+def get_app(key):
+    for app in all_apps:
+        if app['name'] == key:
+            return app
 
 """
 MODELS
 """
 roles_users = db.Table('roles_users',
-        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+                       db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+                       db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
 
 class Role(db.Model, RoleMixin):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -44,22 +61,24 @@ class User(db.Model, UserMixin):
     confirmed_at = db.Column(db.DateTime())
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
-    
+
     def __init__(self, name, active, roles, email, password):
         self.name = name
         self.email = email.lower()
         self.active = active
         self.roles = roles
         self.set_password(password)
-    
+
     def set_password(self, password):
         self.pwdhash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.pwdhash, password)
 
+
 class ExtendedLoginForm(LoginForm):
     name = TextField('Name', [DataRequired()])
+
     def validate(self):
         rv = Form.validate(self)
         if not rv:
@@ -99,6 +118,7 @@ def create_user_and_role():
         except Exception, e:
             the_new_dhbox = DockerBackend.setup_new_dhbox(username, user_pass, user_email)
 
+
 def delete_user(user):
     try:
         user = User.query.filter(User.name == user).first()
@@ -107,53 +127,64 @@ def delete_user(user):
     except Exception, e:
         print e
 
+
 """
 URLS/VIEWS
 """
 
+
 @app.route('/test')
 def test():
     from time import sleep
+
     return render_template('test.html')
-            
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
+
 
 @app.route("/signup")
 def signup():
     return render_template('signup.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
+
 @app.route('/our_team')
 def our_team():
     return render_template('our_team.html')
+
 
 @app.route('/get_started')
 def get_started():
     return render_template('get_started.html')
 
+
 @app.route('/demo', methods=["GET"])
 def demo():
-    username = 'demo'+''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
-    demo_user_object = user_datastore.create_user(email=username+'@demo.com', name=username, password='demo')
+    username = 'demo' + ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+    demo_user_object = user_datastore.create_user(email=username + '@demo.com', name=username, password='demo')
     db.session.commit()
     login_user(demo_user_object)
     new_dhbox = DockerBackend.demo_dhbox(username)
     return username
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     # form = LoginForm()
     # if form.validate_on_submit():
-    #     print 'asdasd'
+    # print 'asdasd'
     #     # login and validate the user...
     #     login_user(user)
     #     flash("Logged in successfully.", 'alert-success')
@@ -168,6 +199,7 @@ def admin():
     containers = DockerBackend.all_containers()
     return render_template('admin.html', containers=containers)
 
+
 @app.route("/dhbox/<the_user>")
 @login_required
 def user_box(the_user):
@@ -179,25 +211,31 @@ def user_box(the_user):
     dhbox_username = which_user.name
     port_info = DockerBackend.get_all_exposed_ports(dhbox_username)
     hostname = DockerBackend.get_hostname()
-    resp = make_response(render_template('my_dhbox.html', user=the_user, apps=all_apps))
+    resp = make_response(render_template('my_dhbox.html',
+                                         user=the_user,
+                                         apps=filter(lambda app: app.get('hide', False) != True, all_apps)
+                                         )
+                         )
     return resp
+
 
 @app.route("/dhbox/<the_user>/<app_name>")
 @login_required
 def app_box(the_user, app_name):
     which_user = User.query.filter(User.name == str(the_user)).first()
     dhbox_username = which_user.name
-    which_app = all_apps[app_name]
-    port_info = DockerBackend.get_container_port(dhbox_username, which_app)
+    app_port = get_app(app_name)['port']
+    port_info = DockerBackend.get_container_port(dhbox_username, app_port)
     hostname = DockerBackend.get_hostname()
-    location = hostname+":"+port_info
-    return redirect('http://'+location)
+    location = hostname + ":" + port_info
+    return redirect('http://' + location)
+
 
 @app.route('/new_dhbox', methods=['POST'])
 def new_dhbox():
     users_and_passes = []
     admins_and_passes = []
-    form  = request.form
+    form = request.form
     data = {key: request.form.getlist(key)[0] for key in request.form.keys()}
     for key in data:
         users_dict = key
@@ -205,7 +243,7 @@ def new_dhbox():
     users = users_dict['users']
     for user in users:
         if 'name' in user:
-            if 'email' in user: # Then is DH Box admin
+            if 'email' in user:  # Then is DH Box admin
                 name_check = User.query.filter(User.name == user['name']).first()
                 email_check = User.query.filter(User.name == user['email']).first()
                 if name_check or email_check:
@@ -220,6 +258,7 @@ def new_dhbox():
                 the_new_dhbox = DockerBackend.setup_new_dhbox(admin_user, admin_pass, admin_email)
     return str('Successfully created a new DH Box.')
 
+
 @app.route('/kill_dhbox', methods=['POST'])
 def kill_dhbox():
     the_next = request.form['next']
@@ -229,13 +268,14 @@ def kill_dhbox():
     flash(message='DH Box and username deleted.', category='alert-success')
     return redirect(url_for(the_next) or url_for("index"))
 
+
 if __name__ == '__main__':
     app.debug = app.config['TESTING']
     # Make database if it doesn't exist
     if not os.path.exists('dhbox-docker.db'):
         db.create_all()
         create_user_and_role()
-	# Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 80))
     app.run(host='0.0.0.0', port=port, threaded=True)
-	# app.run()
+# app.run()
