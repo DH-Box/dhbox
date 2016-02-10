96925a6623f8faa0c23463222867824acc2bd9d1
Stephen Zweibel <srzweibel@gmail.com>
2016-02-10 13:24:35 -0500

    add regex preventing capital letters in usernames

--- DockerBackend.py
+++ DockerBackend.py
@@ -193,6 +193,25 @@ def check_and_kill(user):
     except docker.errors.NotFound, e:
         dhbox.delete_user(user.name)
 
+def display_time(seconds, granularity=2):
+    intervals = (
+    ('weeks', 604800),  # 60 * 60 * 24 * 7
+    ('days', 86400),    # 60 * 60 * 24
+    ('hours', 3600),    # 60 * 60
+    ('minutes', 60),
+    ('seconds', 1),
+    )
+    result = []
+    for name, count in intervals:
+        value = seconds // count
+        if value:
+            seconds -= value * count
+            if value == 1:
+                name = name.rstrip('s')
+            result.append("{} {}".format(value, name))
+    return ', '.join(result[:granularity])
+
+
 
 c = attach_to_docker_client()
 
