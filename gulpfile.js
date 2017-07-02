3243e558934aed034ca6de3e2adf5cca4ea342a5
Jonathan Reeve <jon.reeve@gmail.com>
2017-07-02 17:34:43 +0000

    starting to reconfigure gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp'),
-    useref = require('gulp-useref'),
-    gulpif = require('gulp-if'),
-    uglify = require('gulp-uglify'),
-    minifyCss = require('gulp-minify-css'),
-    del = require('del'),
-    less = require('gulp-less');
+    del  = require('del')
+
+// Dynamically load plugins.
+var plugins = require("gulp-load-plugins")({
+	pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
+	replaceString: /\bgulp[\-.]/
+});
+
+var dest = 'dist/'
 
 gulp.task('clean', function () {
     del.sync(['dist']);
@@ -21,6 +24,23 @@ gulp.task('useref', ['clean'], function () {
         .pipe(useref())
         .pipe(gulp.dest('dist'))
 });
+
+gulp.task('js', function () { 
+	var jsFiles = 'src/static/js/*.js'
+	gulp.src(plugins.mainBowerFiles().concat(jsFiles))
+	.pipe(plugins.filter('*.js'))
+	//.pipe(plugins.concat('main.js'))
+	//.pipe(plugins.uglify())
+	.pipe(gulp.dest('dist/static/js'));
+});
+
+gulp.task('css', function () { 
+	var lessFiles = 'src/static/css/*.less'
+	gulp.src(plugins.mainBowerFiles().concat(lessFiles))
+        .pipe(plugins.if('*.less', plugins.less()))
+	.pipe(plugins.concat('dhbox.css'))
+	.pipe(gulp.dest(dest + 'css'));
+}); 
 
 gulp.task('copy-static', ['copy-images', 'copy-fonts', 'copy-other'], function () {
 
